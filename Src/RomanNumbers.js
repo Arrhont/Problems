390827ccdc3821248b772ac7b7ee3f8c9fe97e9d
EvgS <arrhont.es@gmail.com>
2019-09-16 22:14:30 +0300

    Prettier test

--- Src/RomanNumbers.js
+++ Src/RomanNumbers.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 
-let rNum1 = 'XVI';
-let rNum2 = 'XIX';
-let rNum3 = 'XXIV';
-let rNum4 = 'III';
-let rNum5 = 'CXCIX';
-let rNum6 = 'CX';
+let rNum1 = "XVI";
+let rNum2 = "XIX";
+let rNum3 = "XXIV";
+let rNum4 = "III";
+let rNum5 = "CXCIX";
+let rNum6 = "CX";
 
 let romanConverter = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-    W: 5000,
-    Z: 10000,
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+  W: 5000,
+  Z: 10000
 };
 
 function romanToArabic(str) {
-    let romanArray = str.split('');
-    let arabicArray = romanArray.map( (a) => romanConverter[a] );
+  let romanArray = str.split("");
+  let arabicArray = romanArray.map(a => romanConverter[a]);
 
-    return arabicArray.reduce((acc, current, index) => {
-        if ((current >= arabicArray[index + 1]) || index === arabicArray.length - 1) {
-            acc += current;
-        } else {
-            acc -= current;
-        }
+  return arabicArray.reduce((acc, current, index) => {
+    if (current >= arabicArray[index + 1] || index === arabicArray.length - 1) {
+      acc += current;
+    } else {
+      acc -= current;
+    }
 
-        return acc;
-    }, 0);
+    return acc;
+  }, 0);
 }
 
 console.log(romanToArabic(rNum1));
@@ -42,35 +42,71 @@ console.log(romanToArabic(rNum5));
 console.log(romanToArabic(rNum6));
 
 function arabicToRoman(num) {
-    let arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000, 4000, 5000, 9000, 10000];
-    let roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M', 'MW', 'W', 'WZ', 'Z'];
+  let arabic = [
+    1,
+    4,
+    5,
+    9,
+    10,
+    40,
+    50,
+    90,
+    100,
+    400,
+    500,
+    900,
+    1000,
+    4000,
+    5000,
+    9000,
+    10000
+  ];
+  let roman = [
+    "I",
+    "IV",
+    "V",
+    "IX",
+    "X",
+    "XL",
+    "L",
+    "XC",
+    "C",
+    "CD",
+    "D",
+    "CM",
+    "M",
+    "MW",
+    "W",
+    "WZ",
+    "Z"
+  ];
 
-    function arrayConverterCreate(obj) {
-        let baseArray = [1, 4, 5, 9];
-        let maxRomanLetterValue = Math.max(...Object.values(obj));
+  function arrayConverterCreate(obj) {
+    let baseArray = [1, 4, 5, 9];
+    let maxRomanLetterValue = Math.max(...Object.values(obj));
 
-        for (let i = 1; i < maxRomanLetterValue; i *= 10) {
-            arabic.push(baseArray.map((item) => item * i));
-        }
-        arabic.push(maxRomanLetterValue);
-    } ///FAIL((
-
-    if (!isFinite(num)) return 'Ошибка, не число.';
-    if (num > 39999) return 'Сильно много';
-
-    let result = '';
-    let i = arabic.length - 1;
-    let remainNum = num;
-
-    while (remainNum > 0) {
-        if (remainNum >= arabic[i]) {
-            result += roman[i];
-            remainNum -= arabic[i];
-        } else {
-            i--;
-        }
+    for (let i = 1; i < maxRomanLetterValue; i *= 10) {
+      arabic.push(baseArray.map(item => item * i));
     }
-    return result;
+    arabic.push(maxRomanLetterValue);
+  } ///FAIL((
+
+  if (!isFinite(num)) return "Ошибка, не число.";
+  if (num > 39999) return "Сильно много";
+
+  let result = "";
+  let i = arabic.length - 1;
+  let remainNum = num;
+
+  while (remainNum > 0) {
+    if (remainNum >= arabic[i]) {
+      result += roman[i];
+      remainNum -= arabic[i];
+    } else {
+      i--;
+    }
+  }
+  return result;
 }
 
 console.log(arabicToRoman(459));
