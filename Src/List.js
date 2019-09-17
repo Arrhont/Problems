4e9a392284e3ca336132b3d479149ac6823d2708
EvgS <arrhont.es@gmail.com>
2019-09-18 00:01:44 +0300

    Fixes

--- Src/List.js
+++ Src/List.js
@@ -12,32 +12,40 @@ let list = {
 };
 
 function arrayToList(arr) {
-//   let list = { value: null, rest: null };
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     list.value = arr[i];
-//     if (!i) return list;
-//     list.rest = list;
-//   }
-  let objArr = arr.map( (item) => {value : item, rest : null} );
-  for (let i = objArr.length -1; i < 1; i--) {
-      objArr[i - 1].rest = objArr[i];
-  }
-  return objArr[0];
+  return arr.reduceRight((acc, item) => {
+    let temp = { value: item, rest: acc };
+    return temp;
+  }, null);
+
+  // let objArr = arr.map( (item) => ({value : item, rest : null}) );
+  // for (let i = objArr.length -1; i >= 1; i--) {
+  //     objArr[i - 1].rest = objArr[i];
+  // }
+  // return objArr[0];
 }
 
 console.log(list);
 console.log(arrayToList([1, 2, 3])); // тесты
-console.log(list.rest.rest.value); // тесты
-console.log(list.rest.rest.rest); // тесты
 
 function listToArray(list) {
   let result = [];
   result.push(list.value);
   if (list.rest) {
-    listToArray(list.rest);
+    result.push(...listToArray(list.rest));
   }
   return result;
 }
 
-let test = arrayToList([1, 2]);
 console.log(listToArray(list));
+
+function listToArrayLoop(list) {
+  let result = [];
+  let temp = list;
+  while (temp) {
+    result.push(temp.value);
+    temp = temp.rest;
+  }
+  return result;
+}
+
+console.log(listToArrayLoop(list));
