0ed6e47c6411a104dc2746ac600ad01ecbc873ae
Селянин Евгений Михайлович <selyanin.em@elektro.local>
2019-09-19 17:28:52 +0300

    Some ideas

--- Src/ReversePolishConverter.js
+++ Src/ReversePolishConverter.js
@@ -1,11 +1,31 @@
 'use strict';
 
-import {reversePolishCalc} from './ReversePolishCalc.js';
+import { reversePolishCalc } from './ReversePolishCalc.js';
 
 export function ReversePolishConverter(str) {
-    
-}
+    const priority = {
+        '(': 1,
+        '+': 2,
+        '-': 2,
+        '*': 3,
+        '/': 4,
+    };
 
+    let inputArray = str.split('');
+    if (str[0] == '-') {
+        inputArray.unshift('0');
+    }
+
+    let result = '';
+    let operatorStack = [];
+
+    for (let i = 0; i < inputArray.length; i++) {
+        if (isFinite(inputArray[i])) {
+            result = result + inputArray[i];
+        }
+
+    }
+}
 
 let test;
 console.log(reversePolishCalc(test));
